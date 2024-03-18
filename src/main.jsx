248eaaff7b2f5e684435ce4d94c55fa3191ad224
Weldegebrial Belete <weldegebrial.gesesse@gmail.com>
2024-03-18 08:48:41 +0300

    first commi

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 import './components/interceptors/axios'
 import { Provider } from "react-redux";
-import store from "../../zegeju-12/src/components/OTP/store.jsx";
+import store from "./components/OTP/store.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <Provider store={store}>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
-    
+
   </React.StrictMode>,
 )
